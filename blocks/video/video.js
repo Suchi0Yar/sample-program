e97fff4cb843595b8808453dad87792c36d4472e
Suchi0Yar <syaragudri@adobe.com>
2025-02-12 15:09:11 +0530

    added css

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,10 +1,22 @@
-function embedVideo(link) {
-    const embedLink = link;
+function insertVideo(link) {
+    if (!link) {
+        console.error('Invalid video link');
+        return;
+    }
+    
     const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', embedLink);
+    iframe.src = link;
+    iframe.classList.add('video-iframe');
+    
     const videoDiv = document.querySelector('.video');
+    if (!videoDiv) {
+        console.error('Video container not found');
+        return;
+    }
+    
     videoDiv.innerHTML = '';
     videoDiv.appendChild(iframe);
 }
+
 const driveLink = 'https://drive.google.com/file/d/1prFO2cRCUsLezTLqddQK4a2repB4AWAu/preview';
-embedVideo(driveLink);
+insertVideo(driveLink);
