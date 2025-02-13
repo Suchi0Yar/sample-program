ca4420c485e6a0c826bc461d060250eafca54c30
Suchi0Yar <syaragudri@adobe.com>
2025-02-13 15:17:10 +0530

    added path

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
     try {
-      const response = await fetch("https://main--my-eds-site--sud0202.aem.live/sampledata.json");
+      const response = await fetch("http://localhost:3000/employee.json");
   
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
